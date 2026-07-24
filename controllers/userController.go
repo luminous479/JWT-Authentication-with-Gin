70ed34d2093582d12a4132a9e103097d8c1e6445
luminous479 <aklemasultanakhi@gmail.com>
2026-07-24 22:40:31 +0600

    feat(controllers): add GetUserById endpoint with authorization checks

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,2 +1,42 @@
 package controllers
 
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/luminous479/JWT-Authentication-with-Gin/database"
+	"github.com/luminous479/JWT-Authentication-with-Gin/helpers"
+	"github.com/luminous479/JWT-Authentication-with-Gin/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func GetUserById() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		var user models.User
+
+		err := helpers.MatchUserTypetoUid(c, id)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "User not found"})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		err = userCollection.FindOne(ctx, bson.M{"user_id": id}).Decode(&user)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "User not found"})
+			return
+		}
+
+		c.JSON(200, user)
+	}
+}
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,2 +1,41 @@
 package helpers
 
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("userType")
+
+	if userType != role {
+		err = errors.New("unauthorized to access this resource")
+		return err
+	}
+
+	return nil
+}
+
+func MatchUserTypetoUid(c *gin.Context, role string) (err error) {
+	userType := c.GetString("userType")
+	uid := c.GetString("uid")
+
+	err = nil
+
+	if userType != role {
+
+		err = errors.New("unauthorized to access this resource")
+		return err
+
+	}
+
+	if userType == "user" && uid != c.Param("id") {
+		err = errors.New("unauthorized to access this resource")
+		return err
+	}
+    err = CheckUserType(c, userType)
+	return err
+
+}
+
