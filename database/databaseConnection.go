6f554cf4fee1d03b1d55e353100d5fd82b5de830
luminous479 <aklemasultanakhi@gmail.com>
2026-07-24 21:57:49 +0600

    feat(database): add MongoDB connection initialization

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGODB_URL")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URL is not set")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+
+	log.Println("Connected to MongoDB")
+
+	return client
+}
