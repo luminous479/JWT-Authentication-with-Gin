50c7af4588bcb4e1d96a628edbf9eb7fdac3b9b6
luminous479 <aklemasultanakhi@gmail.com>
2026-07-24 21:44:56 +0600

    feat(routes): register auth and user API routes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luminous479/JWT-Authentication-with-Gin/controllers"
+)
+
+func AuthRouters(Routes *gin.Engine) {
+
+	Routes.POST("users/signUp", controllers.SignUp())
+	Routes.POST("users/signIn", controllers.SignIn())
+
+}
--- routes/userRouters.go
+++ routes/userRouters.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luminous479/JWT-Authentication-with-Gin/controllers"
+	"github.com/luminous479/JWT-Authentication-with-Gin/middelwares"
+)
+
+func UserRoutes(Routes *gin.Engine) {
+	Routes.Use(middelwares.Authenticate())
+	Routes.GET("users/", controllers.GetAllUsers())
+	Routes.GET("users/:id", controllers.GetUserById())
+
+}
